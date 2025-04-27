189040753936bfe4908f89d3f05fc4ed5599c675
k.kotov <k.kotov@bftcom.com>
2025-04-27 19:51:55 +0300

    removed api prefix from axios

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const api = axios.create({
     baseURL: process.env.NODE_ENV === 'development' 
       ?  'http://194.87.93.188/' //'http://localhost:8080' 
-      : '/api', // Для production
+      : '/', // Для production
   });
 
   export default api;
